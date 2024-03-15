8e2825dae2d9cc6f3ab17eada5a1ecc45e1bc1c1
honor_repo <pawan9168kumar@gmail.com>
2024-03-15 22:32:42 +0530

    HDFC_commit

---
+++ HDFC_customer_App-business/src/main/java/in/pawanit1/business/service/pawanitService.java
@@ -0,0 +1,14 @@
+package in.pawanit1.business.service;
+
+/**
+ * pawanit main service interface.
+ * It purposes common methods that will be present in all your services
+ * 
+ * @author Pierre Le Roux
+ */
+public interface pawanitService {
+	
+	/**
+	 * You can put in this interface all the common services you want to purpose for each module which extend it.
+	 */
+}
